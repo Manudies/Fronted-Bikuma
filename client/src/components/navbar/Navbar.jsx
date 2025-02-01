6b518601e9ef61d6ae22fa87becc55d129b8b36f
Manudies <manudies@gmail.com>
2025-02-01 12:14:38 +0100

    feat:navbar responsive

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,33 +1,80 @@
-//import { useState, useEffect, useContext } from 'react';
-import './Navbar.css';
-import Button from '../button/Button';
-//import {button} from './button.css';
+import { useState } from "react";
+import "./Navbar.css";
+import Button from "../button/Button";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <nav className="navbar">
-      <img src="./logotipoo.png" alt="" />
-      <ul className='navbar-list'>
-        <li><a href="/">Servicios</a></li>
-        <li><a href="/">Productos</a></li>
-        <li><a href="/">Administración pública</a></li>
-        <li><a href="/">Conócenos</a></li>
-        <li><a href="/">Actualidad</a></li>
-      </ul>
-      <div className='navbar-buttons'> 
-        <Button children={'Contactar'} className={'contactar'}></Button>
-        <Button children={'Tienda'} className={'tienda'}>
-          <img src="./carro-de-la-compra.png" alt="Logo carrito" />
-          Tienda
-        </Button>
-        <Button className={'lupa'}> 
-          <img src="./search.png" alt="Logo lupa" />
-        </Button>
+    <div>
+      <nav className="navbar">
+        <img src="./logotipoo.png" alt="Logo" />
+        <ul className="navbar-list">
+          <li>
+            <a href="/">Servicios</a>
+          </li>
+          <li>
+            <a href="/">Productos</a>
+          </li>
+          <li>
+            <a href="/">Administración pública</a>
+          </li>
+          <li>
+            <a href="/">Conócenos</a>
+          </li>
+          <li>
+            <a href="/">Actualidad</a>
+          </li>
+        </ul>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <span>☰</span>
+        </button>
+        <div className="navbar-buttons">
+          <Button children={"Contactar"} className={"contactar"}></Button>
+          <Button children={"Tienda"} className={"tienda"}>
+            <img src="./carro-de-la-compra.png" alt="Logo carrito" />
+            Tienda
+          </Button>
+          <Button className={"lupa"}>
+            <img src="./search.png" alt="Logo lupa" />
+          </Button>
+        </div>
+      </nav>
+      {/* Menú desplegable */}
+      <div className={`mobile-menu ${isMenuOpen ? "open" : ""}`}>
+        <div className="mobile-buttons">
+          <Button children={"Contactar"} className={"contactar"}></Button>
+          <Button children={"Tienda"} className={"tienda"}>
+            <img src="./carro-de-la-compra.png" alt="Logo carrito" />
+            Tienda
+          </Button>
+          <Button className={"lupa"}>
+            <img src="./search.png" alt="Logo lupa" />
+          </Button>
+        </div>
+        <ul className="mobile-links">
+          <li>
+            <a href="/">Servicios</a>
+          </li>
+          <li>
+            <a href="/">Productos</a>
+          </li>
+          <li>
+            <a href="/">Administración pública</a>
+          </li>
+          <li>
+            <a href="/">Conócenos</a>
+          </li>
+          <li>
+            <a href="/">Actualidad</a>
+          </li>
+        </ul>
       </div>
-
-
-    </nav>
+    </div>
   );
 };
 
